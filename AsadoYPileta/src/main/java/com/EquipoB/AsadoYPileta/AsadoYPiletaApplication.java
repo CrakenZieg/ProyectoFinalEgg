f75e69ade307b117c38fe725c09a48774e18357f
Francisco Ziegler <franciscoziegler@gmail.com>
2023-10-24 20:42:55 -0300

    primer commit

---
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/AsadoYPiletaApplication.java
@@ -0,0 +1,13 @@
+package com.EquipoB.AsadoYPileta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsadoYPiletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsadoYPiletaApplication.class, args);
+	}
+
+}
