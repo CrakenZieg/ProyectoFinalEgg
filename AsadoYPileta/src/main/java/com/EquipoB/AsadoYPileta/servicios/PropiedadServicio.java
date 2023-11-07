2ce64ae077b2d465c3f9dbf06742f25eea0fe236
“Gabriela <gabrielarivadero03@gmail.com>
2023-11-07 19:13:01 -0300

    cambios isPresent

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/servicios/PropiedadServicio.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/servicios/PropiedadServicio.java
@@ -47,7 +47,7 @@ public class PropiedadServicio {
         
         Optional<Propietario> respuesta = propietarioRepositorio.findById(usuario.getId());
         Propietario propietario = null;
-        if(respuesta.isEmpty()){
+        if(!respuesta.isPresent()){
             propietario = propietarioServicio.crearPropietario(usuario);
         } else {
             propietario = respuesta.get();
