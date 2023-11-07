185cab4caafde93991b065baeb4a427b90d11ddb
Gonzalo <gesellgon137@gmail.com>
2023-11-06 21:02:19 -0300

    Correccion
    Se corrigioeron servicios

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/servicios/PropiedadServicio.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/servicios/PropiedadServicio.java
@@ -22,6 +22,7 @@ public class PropiedadServicio {
 
     @Autowired
     private PropiedadRepositorio propiedadRepositorio;
+
     @Autowired
     private PropietarioServicio propietarioServicio;
     
@@ -30,6 +31,7 @@ public class PropiedadServicio {
     
     @Autowired
     private ServicioServicio servicioServicio;
+
     @Autowired
     private ImagenServicio imagenServicio;
     
@@ -39,7 +41,9 @@ public class PropiedadServicio {
     @Transactional
     public void crearPropiedad(String titulo, String descripcion, String ubicacion, String direccion, TipoPropiedad tipo,
             String[] serviciosInput, MultipartFile[] imagenesInput, Double valor, Usuario usuario) throws MiException, Exception {
+
         validar(titulo, descripcion, ubicacion, direccion, tipo, imagenesInput, valor);
+        
         Optional<Propietario> respuesta = propietarioRepositorio.findById(usuario.getId());
         Propietario propietario = null;
         if(respuesta.isPresent()){
@@ -47,13 +51,16 @@ public class PropiedadServicio {
         } else {
             propietario = propietarioServicio.crearPropietario(usuario);
         }        
+
         List<Servicio> servicios = new ArrayList<>();
         if (serviciosInput != null) {
             servicios = servicioServicio.listarServiciosArray(serviciosInput);
         }
         List<Imagen> imagenes = new ArrayList<>();
         imagenes = imagenServicio.guardarVarias(imagenesInput);
+
         Propiedad propiedad = new Propiedad();
+        
         propiedad.setTitulo(titulo);
         propiedad.setDescripcion(descripcion);
         propiedad.setUbicacion(ubicacion);
@@ -92,36 +99,34 @@ public class PropiedadServicio {
 
             Propiedad propiedad = propiedadRepo.get();
             List<Imagen> imagenes = propiedad.getImagenes();
-
-            if (imagenesViejas != null) {
-                if (imagenesViejas.length != 0) {
-                    imagenes = imagenServicio.filtrar(imagenes,
+            
+            if(imagenesViejas != null){ 
+                if(imagenesViejas.length != 0){
+                    imagenes = imagenServicio.filtrar(imagenes, 
                             imagenesViejas);
                 }
-            }
-            if (imagenesInput != null) {
-                if (imagenesInput.length != 0) {
+            }     
+            if(imagenesInput != null){
+                if(imagenesInput.length != 0){
                     imagenes.addAll(imagenServicio.guardarVarias(imagenesInput));
-                }
+                } 
             }
 
             propiedad.setTitulo(titulo);
             propiedad.setDescripcion(descripcion);
             propiedad.setUbicacion(ubicacion);
-            propiedad.setDireccion(direccion);
-            if ("true".equals(estado)) {
+            propiedad.setDireccion(direccion);            
+            if("true".equals(estado)){
                 propiedad.setEstado(true);
-
-            } else {
-                boolean busqueda = reservaServicio.validarReservasPropiedad(id);
-                if (busqueda == true) {
+            }else{
+                boolean busqueda= reservaServicio.validarReservasPropiedad(id);
+                if(busqueda == true){
                     throw new MiException("No puede darse de baja si tiene reservas activas!");
-                } else {
+                }else{
                     propiedad.setEstado(false);
                 }
-
-            }
-
+             
+           }
             propiedad.setEstado(Boolean.valueOf(estado));
             propiedad.setTipo(tipo);
             propiedad.setServicios(servicios);
@@ -150,7 +155,6 @@ public class PropiedadServicio {
     @Transactional
     public void eliminar(String id, Usuario logueado) {
         Optional<Propiedad> propiedadRepo = propiedadRepositorio.findById(id);
-
         Optional<Propietario> propietarioRepo = propietarioRepositorio.findById(logueado.getId());
         if (propiedadRepo.isPresent() && propietarioRepo.isPresent()){
             Propiedad propiedad = propiedadRepo.get();
@@ -163,26 +167,24 @@ public class PropiedadServicio {
                 propiedadRepositorio.delete(propiedad);
             }
         }        
-
     }
 
     public void validar(String titulo, String descripcion, String ubicacion, String direccion,
             TipoPropiedad tipo, MultipartFile[] imagenes, Double valor) throws MiException {
-
+        
         if (titulo == null || titulo.trim().isEmpty()) {
-
             throw new MiException("El titulo no puede ser nulo o estar vacio");
         }
 
-        if (descripcion == null || descripcion.trim().isEmpty()) {
+        if ( descripcion == null || descripcion.trim().isEmpty() ) {
             throw new MiException("La descripcion no puede ser nulo o estar vacio");
         }
 
-        if (ubicacion == null || ubicacion.trim().isEmpty()) {
+        if (ubicacion == null || ubicacion.trim().isEmpty() ) {
             throw new MiException("La ubicacion no puede ser nulo o estar vacio");
         }
 
-        if (direccion == null || direccion.trim().isEmpty()) {
+        if ( direccion == null || direccion.trim().isEmpty() ) {
             throw new MiException("La direccion no puede ser nulo o estar vacio");
         }
 
@@ -198,5 +200,6 @@ public class PropiedadServicio {
             throw new MiException("El valor no puede ser 0");
         }
     }
+
 
 }
