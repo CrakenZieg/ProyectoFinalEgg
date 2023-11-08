01e07a0015d4cad0951befff89c896cbd3a83e8d
Gonzalo <gesellgon137@gmail.com>
2023-11-07 21:47:11 -0300

    Se agrego vista perfil
    eliminar comentario y se edito un poco comentario servico

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/servicios/ComentarioServicio.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/servicios/ComentarioServicio.java
@@ -29,7 +29,7 @@ public class ComentarioServicio {
 
     @Transactional
     public void crearComentario(HttpSession session, MultipartFile[] archivos, String cuerpo, String stringIdpropiedad) throws MiException, Exception {
-        validar(session,cuerpo, archivos, stringIdpropiedad);
+        validar(session, cuerpo, archivos, stringIdpropiedad);
 
         Comentario comentario = new Comentario();
         comentario.setCuerpo(cuerpo);
@@ -44,7 +44,6 @@ public class ComentarioServicio {
         comentarioRepositorio.save(comentario);
 
     }
-    
 
     public List<Comentario> listarComentario() {
 
@@ -54,12 +53,13 @@ public class ComentarioServicio {
 
         return comentarios;
     }
+
     public List<Comentario> findComentariosByUserId(String userId) {
         return comentarioRepositorio.buscarPorCliente(userId);
     }
 
     public void modificarComentario(HttpSession session, MultipartFile[] archivos, String id, String cuerpo, String stringIdpropiedad, String[] imagenesViejas) throws MiException, Exception {
-        validar(session,cuerpo, archivos, stringIdpropiedad);
+        validar(session, cuerpo, archivos, stringIdpropiedad);
         Optional<Comentario> respuesta = comentarioRepositorio.findById(id);
         if (respuesta.isPresent()) {
 
@@ -94,6 +94,11 @@ public class ComentarioServicio {
         return comentarioRepositorio.getById(id);
     }
 
+    @Transactional
+    public void eliminarComentrario(String id) throws MiException {
+        Comentario comentario = comentarioRepositorio.getById(id);
+        comentarioRepositorio.delete(comentario);
+    }
 
     private void validar(HttpSession session, String cuerpo, MultipartFile[] imagenes, String stringIdpropiedad) throws MiException {
         Usuario logueado = (Usuario) session.getAttribute("usuariosession");
@@ -102,20 +107,20 @@ public class ComentarioServicio {
         }
         if (cuerpo.isEmpty() || cuerpo == null) {
 
-        if ( cuerpo == null || cuerpo.trim().isEmpty() ) {
+            if (cuerpo == null || cuerpo.trim().isEmpty()) {
 
-            throw new MiException("el comentario no puede ser nulo o estar vacío");
-        }
-        if (imagenes.length == 0 || imagenes == null) {
-            throw new MiException("Las imagenes no puede ser nulas o estar vacias");
-        }
+                throw new MiException("el comentario no puede ser nulo o estar vacío");
+            }
+            if (imagenes.length == 0 || imagenes == null) {
+                throw new MiException("Las imagenes no puede ser nulas o estar vacias");
+            }
 
-        if (stringIdpropiedad == null|| stringIdpropiedad.trim().isEmpty()  ) {
-            throw new MiException("la propiedad no puede  estar vacia");
+            if (stringIdpropiedad == null || stringIdpropiedad.trim().isEmpty()) {
+                throw new MiException("la propiedad no puede  estar vacia");
+
+            }
 
         }
 
     }
-
-}
 }
