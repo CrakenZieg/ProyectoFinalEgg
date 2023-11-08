bebdda70be79fb2543bc5abdd4e223888b5032b0
Francisco Ziegler <franciscoziegler@gmail.com>
2023-11-08 20:12:39 -0300

    tipoPropiedad en index

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/PortalControlador.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/PortalControlador.java
@@ -1,8 +1,9 @@
 package com.EquipoB.AsadoYPileta.controladores;
 
 import com.EquipoB.AsadoYPileta.entidades.Propiedad;
-import com.EquipoB.AsadoYPileta.enumeraciones.TipoPropiedad;
+import com.EquipoB.AsadoYPileta.entidades.TipoPropiedad;
 import com.EquipoB.AsadoYPileta.servicios.PropiedadServicio;
+import com.EquipoB.AsadoYPileta.servicios.TipoPropiedadServicio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,13 +19,15 @@ public class PortalControlador {
     @Autowired
     private PropiedadServicio propiedadServicio;
     
-    private TipoPropiedad tipos;
+    @Autowired
+    private TipoPropiedadServicio tipopropiedadServicio;
     
     @GetMapping("/")
     public String index(ModelMap model){        
         List<Propiedad> propiedades = propiedadServicio.listarPropiedades();
+        List<TipoPropiedad> tipoPropiedades = tipopropiedadServicio.listarTipoPropiedad();
         model.addAttribute("propiedades", propiedades);        
-        model.addAttribute("tipos", tipos);
+        model.addAttribute("tipoPropiedades", tipoPropiedades);
         return "index.html";
     }
     
