594abe7efbba631a354a4c5a62c4faaa54bc8783
Gonzalo <gesellgon137@gmail.com>
2023-11-14 20:36:05 -0300

    loguearse para poder hacer reservas

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/ReservaControlador.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/ReservaControlador.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,7 +36,8 @@ public class ReservaControlador {
     private UsuarioServicio usuarioServicio;
     @Autowired
     private ClienteServicio clienteServicio;
-
+    
+    @PreAuthorize("hasAnyRole('ROLE_CLIENTE','ROLE_PROPIETARIO')")
     @PostMapping("/registrar")  //localhost:8080/reserva/registrar
     public ModelAndView crearReserva(@RequestParam String idPropiedad, @RequestParam String fechaInicio,
             @RequestParam String fechaFinal, HttpSession session, ModelMap modelo)  {
