f26e17d4b894bd2088fb4b30e33f7bb94d82220b
tamara zabala <tamara.zzabala@gmail.com>
2023-11-14 21:10:15 -0300

    modificacion de rol en cliente serv

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/ClienteControlador.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/ClienteControlador.java
@@ -59,10 +59,10 @@ public class ClienteControlador {
     public String registro(@RequestParam String nombre, @RequestParam String apellido,
             @RequestParam String email, @RequestParam String password, @RequestParam String password2,
             @RequestParam MultipartFile[] imagenesInput, @RequestParam String descripcion,
-            @RequestParam String[] tipoContactoInput, @RequestParam String[] contactosInput) {
+            @RequestParam String[] tipoContactoInput, @RequestParam String[] contactosInput,@RequestParam String rol) {
         try {
             clienteServicio.crearCliente(email, nombre, apellido, descripcion,
-                    password, password2, imagenesInput, tipoContactoInput, contactosInput);
+                    password, password2, imagenesInput, tipoContactoInput, contactosInput,rol);
         } catch (Exception ex) {
             Logger.getLogger(ClienteControlador.class.getName()).log(Level.SEVERE, null, ex);
         }
