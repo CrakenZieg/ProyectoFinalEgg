b3eb9ac0b9a4203f8e2d1bebe5b8ce45b31cdbb6
matudelbosque <matudelbosque@gmail.com>
2023-12-18 22:12:26 -0300

    arreglos con main

--- AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/ClienteControlador.java
+++ AsadoYPileta/src/main/java/com/EquipoB/AsadoYPileta/controladores/ClienteControlador.java
@@ -1,8 +1,6 @@
 package com.EquipoB.AsadoYPileta.controladores;
 
 import com.EquipoB.AsadoYPileta.entidades.Cliente;
-
-
 import com.EquipoB.AsadoYPileta.entidades.Propietario;
 import com.EquipoB.AsadoYPileta.entidades.Reserva;
 import com.EquipoB.AsadoYPileta.entidades.Usuario;
@@ -47,7 +45,6 @@ public class ClienteControlador {
     
     @Autowired
     private ReservaServicio reservaServicio;
-
 
 
     @GetMapping("/registrar")
